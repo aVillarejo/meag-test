b1d71ba07cd5bef6bcaf85ad55fb89a5e7896317
“a_villarejo19” <a.villarejo19@gmail.com>
2019-04-10 04:21:01 -0500

    .

--- Server.js
+++ Server.js
@@ -11,7 +11,7 @@ SERVER.applyMiddleware({
 // const PORT = process.env.PORT || 4000;
 // Express: Listener
 
-SERVER.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+app.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
 
