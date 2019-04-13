1756fb57d569705f8b8226278ac82b3975203d7e
“a_villarejo19” <a.villarejo19@gmail.com>
2019-04-12 21:04:51 -0500

    connect db test 3

--- db/index.js
+++ db/index.js
@@ -7,9 +7,11 @@ if (process.env.MONGO_USER && process.env.MONGO_PASSWORD) {
   mongoUserCredentials = `${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@`;
 }
 
+//const URI="mongodb+srv://AdminAV:<password>@clusterav-qx4cd.mongodb.net/test?retryWrites=true"
+
 const MONGO_URL = process.env.MONGO_URL ;
 const DB_NAME = process.env.MONGO_DB_NAME; 
-const MONGO_CONNECTION_STRING = `mongodb+srv//${mongoUserCredentials}${MONGO_URL}/${DB_NAME}`;
+const MONGO_CONNECTION_STRING = `mongodb+srv://${mongoUserCredentials}${MONGO_URL}/${DB_NAME}`;
 
 mongoose.connect(MONGO_CONNECTION_STRING, { useNewUrlParser: true });
 mongoose.set("findAndModify", false);
