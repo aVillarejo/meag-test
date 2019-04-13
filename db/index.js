40f5c22eb97b3904397076ceec094d6e96131f2f
“a_villarejo19” <a.villarejo19@gmail.com>
2019-04-12 20:51:27 -0500

    db connection test

--- db/index.js
+++ db/index.js
@@ -4,18 +4,14 @@ mongoose.Promise = global.Promise;
 
 let mongoUserCredentials = "";
 if (process.env.MONGO_USER && process.env.MONGO_PASSWORD) {
-  mongoUserCredentials = `${process.env.MONGO_USER}:${
-    process.env.MONGO_PASSWORD
-  }@`;
+  mongoUserCredentials = `${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@`;
 }
-const URI =
-  "mongodb+srv://AdminAV:soporte123$@clusterav-qx4cd.mongodb.net/SISDE?retryWrites=true";
 
-const MONGO_URL = process.env.MONGO_URL || "localhost:27017";
-const DB_NAME = process.env.MONGO_DB_NAME || "sis-db";
+const MONGO_URL = process.env.MONGO_URL || "clusterav-qx4cd.mongodb.net";
+const DB_NAME = process.env.MONGO_DB_NAME || "CRM_DB";
 const MONGO_CONNECTION_STRING = `mongodb://${mongoUserCredentials}${MONGO_URL}/${DB_NAME}`;
 
-mongoose.connect(URI, { useNewUrlParser: true });
+mongoose.connect(MONGO_CONNECTION_STRING, { useNewUrlParser: true });
 mongoose.set("findAndModify", false);
 
 //#region schemas
